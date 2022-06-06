169cdb67736a04afe3a45155631897c702af7e08
Doan Quang Huy <quanghuybest@gmail.com>
2022-06-06 21:14:02 +0700

    upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,363 @@
+﻿
+namespace QuanLyDienJAV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtgvModelJav = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_id = new System.Windows.Forms.TextBox();
+            this.txt_fullname = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_YearofBirth = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_film = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_nationality = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.btn_save = new System.Windows.Forms.Button();
+            this.btn_update = new System.Windows.Forms.Button();
+            this.btn_delete = new System.Windows.Forms.Button();
+            this.group_info = new System.Windows.Forms.GroupBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fullname = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.YearofBirth = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.film = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nationality = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvModelJav)).BeginInit();
+            this.group_info.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dtgvModelJav
+            // 
+            this.dtgvModelJav.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dtgvModelJav.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgvModelJav.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.fullname,
+            this.YearofBirth,
+            this.film,
+            this.nationality});
+            this.dtgvModelJav.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dtgvModelJav.Location = new System.Drawing.Point(0, 0);
+            this.dtgvModelJav.Name = "dtgvModelJav";
+            this.dtgvModelJav.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dtgvModelJav.Size = new System.Drawing.Size(715, 247);
+            this.dtgvModelJav.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Rockwell", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(250, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(158, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Quản lý Idol JAV";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(28, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "ID:";
+            // 
+            // txt_id
+            // 
+            this.txt_id.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_id.Location = new System.Drawing.Point(101, 20);
+            this.txt_id.Name = "txt_id";
+            this.txt_id.Size = new System.Drawing.Size(250, 21);
+            this.txt_id.TabIndex = 3;
+            // 
+            // txt_fullname
+            // 
+            this.txt_fullname.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_fullname.Location = new System.Drawing.Point(101, 46);
+            this.txt_fullname.Name = "txt_fullname";
+            this.txt_fullname.Size = new System.Drawing.Size(250, 21);
+            this.txt_fullname.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(28, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Họ và tên:";
+            // 
+            // txt_YearofBirth
+            // 
+            this.txt_YearofBirth.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_YearofBirth.Location = new System.Drawing.Point(101, 73);
+            this.txt_YearofBirth.Name = "txt_YearofBirth";
+            this.txt_YearofBirth.Size = new System.Drawing.Size(250, 21);
+            this.txt_YearofBirth.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(28, 76);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 15);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Năm sinh:";
+            // 
+            // txt_film
+            // 
+            this.txt_film.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_film.Location = new System.Drawing.Point(101, 101);
+            this.txt_film.Name = "txt_film";
+            this.txt_film.Size = new System.Drawing.Size(250, 21);
+            this.txt_film.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(28, 104);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 15);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Bộ phim:";
+            // 
+            // txt_nationality
+            // 
+            this.txt_nationality.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_nationality.Location = new System.Drawing.Point(101, 128);
+            this.txt_nationality.Name = "txt_nationality";
+            this.txt_nationality.Size = new System.Drawing.Size(250, 21);
+            this.txt_nationality.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(28, 131);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 15);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Quốc tịch:";
+            // 
+            // btn_add
+            // 
+            this.btn_add.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btn_add.Location = new System.Drawing.Point(240, 10);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(97, 23);
+            this.btn_add.TabIndex = 12;
+            this.btn_add.Text = "Thêm diễn viên";
+            this.btn_add.UseVisualStyleBackColor = true;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
+            // 
+            // btn_save
+            // 
+            this.btn_save.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btn_save.Location = new System.Drawing.Point(357, 10);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(98, 23);
+            this.btn_save.TabIndex = 13;
+            this.btn_save.Text = "Lưu thay đổi";
+            this.btn_save.UseVisualStyleBackColor = true;
+            this.btn_save.Click += new System.EventHandler(this.btn_save_Click);
+            // 
+            // btn_update
+            // 
+            this.btn_update.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btn_update.Location = new System.Drawing.Point(486, 10);
+            this.btn_update.Name = "btn_update";
+            this.btn_update.Size = new System.Drawing.Size(91, 23);
+            this.btn_update.TabIndex = 14;
+            this.btn_update.Text = "Sửa thông tin";
+            this.btn_update.UseVisualStyleBackColor = true;
+            this.btn_update.Click += new System.EventHandler(this.btn_update_Click);
+            // 
+            // btn_delete
+            // 
+            this.btn_delete.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btn_delete.Location = new System.Drawing.Point(601, 10);
+            this.btn_delete.Name = "btn_delete";
+            this.btn_delete.Size = new System.Drawing.Size(91, 23);
+            this.btn_delete.TabIndex = 15;
+            this.btn_delete.Text = "Xóa diễn viên";
+            this.btn_delete.UseVisualStyleBackColor = true;
+            this.btn_delete.Click += new System.EventHandler(this.btn_delete_Click);
+            // 
+            // group_info
+            // 
+            this.group_info.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.group_info.Controls.Add(this.txt_nationality);
+            this.group_info.Controls.Add(this.label6);
+            this.group_info.Controls.Add(this.txt_film);
+            this.group_info.Controls.Add(this.label5);
+            this.group_info.Controls.Add(this.txt_YearofBirth);
+            this.group_info.Controls.Add(this.label4);
+            this.group_info.Controls.Add(this.txt_fullname);
+            this.group_info.Controls.Add(this.label3);
+            this.group_info.Controls.Add(this.txt_id);
+            this.group_info.Controls.Add(this.label2);
+            this.group_info.Location = new System.Drawing.Point(0, 56);
+            this.group_info.Name = "group_info";
+            this.group_info.Size = new System.Drawing.Size(715, 159);
+            this.group_info.TabIndex = 16;
+            this.group_info.TabStop = false;
+            this.group_info.Text = "Thông tin của diễn viên:";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(715, 50);
+            this.panel1.TabIndex = 12;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.dtgvModelJav);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 251);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(715, 247);
+            this.panel2.TabIndex = 17;
+            // 
+            // panel3
+            // 
+            this.panel3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel3.Controls.Add(this.btn_update);
+            this.panel3.Controls.Add(this.btn_add);
+            this.panel3.Controls.Add(this.btn_save);
+            this.panel3.Controls.Add(this.btn_delete);
+            this.panel3.Location = new System.Drawing.Point(0, 211);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(715, 40);
+            this.panel3.TabIndex = 18;
+            // 
+            // ID
+            // 
+            this.ID.DataPropertyName = "id";
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            // 
+            // fullname
+            // 
+            this.fullname.DataPropertyName = "fullname";
+            this.fullname.HeaderText = "Full Name";
+            this.fullname.Name = "fullname";
+            // 
+            // YearofBirth
+            // 
+            this.YearofBirth.DataPropertyName = "YearofBirth";
+            this.YearofBirth.HeaderText = "Năm sinh";
+            this.YearofBirth.Name = "YearofBirth";
+            // 
+            // film
+            // 
+            this.film.DataPropertyName = "film";
+            this.film.HeaderText = "Bộ phim";
+            this.film.Name = "film";
+            // 
+            // nationality
+            // 
+            this.nationality.DataPropertyName = "nationality";
+            this.nationality.HeaderText = "Quốc tịch";
+            this.nationality.Name = "nationality";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(715, 498);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.group_info);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Quản lý diễn viên Jav";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvModelJav)).EndInit();
+            this.group_info.ResumeLayout(false);
+            this.group_info.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dtgvModelJav;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_id;
+        private System.Windows.Forms.TextBox txt_fullname;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_YearofBirth;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_film;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt_nationality;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.Button btn_save;
+        private System.Windows.Forms.Button btn_update;
+        private System.Windows.Forms.Button btn_delete;
+        private System.Windows.Forms.GroupBox group_info;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fullname;
+        private System.Windows.Forms.DataGridViewTextBoxColumn YearofBirth;
+        private System.Windows.Forms.DataGridViewTextBoxColumn film;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nationality;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace QuanLyDienJAV
+{
+    public partial class Form1 : Form
+    {
+        private XDocument xmldoc;
+        private string path = "DienVienJAV.xml";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadDataModelJAV();
+        }
+        private void ClearDataBiding()
+        {
+            txt_id.DataBindings.Clear();
+            txt_fullname.DataBindings.Clear();
+            txt_YearofBirth.DataBindings.Clear();
+            txt_film.DataBindings.Clear();
+            txt_nationality.DataBindings.Clear();
+        }
+        public void LoadDataModelJAV()
+        {
+            xmldoc = XDocument.Load(path);
+            var data = xmldoc.Descendants("ModelJAV").Select(p => new
+            {
+                Id = p.Element("id").Value,
+                FullName = p.Element("fullname").Value,
+                YearofBirth = p.Element("YearofBirth").Value,
+                film = p.Element("film").Value,
+                nationality = p.Element("nationality").Value
+            }).OrderBy(p => p.Id).ToList();
+            //lam sach du lieu
+            ClearDataBiding();
+            // them du lieu vao dtgv
+            txt_id.DataBindings.Add("text", data, "id");
+            txt_fullname.DataBindings.Add("text", data, "fullname");
+            txt_YearofBirth.DataBindings.Add("text", data, "YearofBirth");
+            txt_film.DataBindings.Add("text", data, "film");
+            txt_nationality.DataBindings.Add("text", data, "nationality");
+            dtgvModelJav.DataSource = data;
+        }
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            foreach (var item in group_info.Controls)
+            {
+                if (item is TextBox)
+                {
+                    (item as TextBox).Clear();
+                }
+            }
+            txt_id.Focus();
+        }
+
+        private void btn_save_Click(object sender, EventArgs e)
+        {
+            XElement models = new XElement("ModelJAV",
+                new XElement("id", txt_id.Text),
+                new XElement("fullname", txt_fullname.Text),
+                new XElement("YearofBirth", txt_YearofBirth.Text),
+                new XElement("film", txt_film.Text),
+                new XElement("nationality", txt_nationality.Text));
+            xmldoc.Root.Add(models);
+            xmldoc.Save(path);
+            LoadDataModelJAV();
+            btn_add_Click(null, null);
+        }
+
+        private void btn_update_Click(object sender, EventArgs e)
+        {
+            XElement models = xmldoc.Descendants("Employee").FirstOrDefault(p => p.Element("id").Value == txt_id.Text);
+            if (models != null)
+            {
+                models.Element("fullname").Value = txt_fullname.Text;
+                models.Element("YearofBirth").Value = txt_YearofBirth.Text;
+                models.Element("film").Value = txt_film.Text;
+                models.Element("nationality").Value = txt_nationality.Text;
+                xmldoc.Save(path);
+                LoadDataModelJAV();
+                btn_add_Click(null, null);
+            }
+        }
+
+        private void btn_delete_Click(object sender, EventArgs e)
+        {
+            XElement models = xmldoc.Descendants("ModelJAV").FirstOrDefault(p => p.Element("id").Value == txt_id.Text);
+            if (models != null)
+            {
+                models.Remove();
+                xmldoc.Save(path);
+                LoadDataModelJAV();
+                btn_add_Click(null, null);
+            }
+        }
+    }
+}
